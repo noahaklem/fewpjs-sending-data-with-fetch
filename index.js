15f1c7422c0eb8dc2856d9844ad788f3ffe4df96
Noah A Klem <noahaklem@gmail.com>
2021-11-01 19:14:13 -0600

    commit tree edf13069a903f343fa841a45bdf0547fa5a9f8bc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const configObj = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    });
+
+};
